f65a68a7b59d92f9b90ff80f1c71920f50f18990
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2022-12-30 06:11:35 +0700

    add : empty screen when data not available

--- app/src/main/java/com/drsync/listcharacter/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/drsync/listcharacter/ui/screen/home/HomeScreen.kt
@@ -1,18 +1,24 @@
 package com.drsync.listcharacter.ui.screen.home
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.drsync.listcharacter.R
 import com.drsync.listcharacter.data.entity.Character
 import com.drsync.listcharacter.di.Injection
 import com.drsync.listcharacter.ui.common.UiState
@@ -56,24 +62,40 @@ fun HomeContent(
     searchCharacter: (String) -> Unit,
     query: String? = null,
 ) {
-    LazyColumn(
-        contentPadding = PaddingValues(16.dp),
-        modifier = modifier.testTag("CharacterList")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        item {
-            SearchBar(
-                query = query as String,
-                onValueChange = searchCharacter,
-                modifier = Modifier.padding(bottom = 8.dp)
+        SearchBar(
+            query = query as String,
+            onValueChange = searchCharacter,
+            modifier = Modifier.padding(
+                bottom = 8.dp,
+                top = 16.dp,
+                start = 16.dp,
+                end = 16.dp
             )
-        }
-        items(characters) { data ->
-            CharacterItem(
-                image = data.imageUrl, name = data.name,
-                modifier = Modifier.clickable {
-                    navigateToDetail(data.id)
+        )
+        if (characters.isEmpty()) {
+            Text(
+                text = stringResource(id = R.string.no_chara_found),
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        } else {
+            LazyColumn(
+                contentPadding = PaddingValues(16.dp),
+                modifier = modifier.testTag("CharacterList")
+            ) {
+                items(characters) { data ->
+                    CharacterItem(
+                        image = data.imageUrl,
+                        name = data.name,
+                        modifier = Modifier.clickable {
+                            navigateToDetail(data.id)
+                        }
+                    )
                 }
-            )
+            }
         }
     }
 }
